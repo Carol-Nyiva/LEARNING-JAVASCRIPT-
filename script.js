20d256b069b9d146172124f3ad847ff9bee4898a
Carol Nyiva <45183823+Carol-Nyiva@users.noreply.github.com>
2020-05-24 19:20:35 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,118 @@
+console.log(' using visual studio to learn JAVASCRIPT ');
+
+/*
+......................................................................................
+
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith'; // string
+var age = '28';         //number
+
+var fullAge = true; //boolean
+console.log(fullAge);
+
+var job; // Variable declared and no value assigned to it 
+console.log(job); //The output will be undefined
+
+job = 'Teacher';
+console.log(job);
+
+var $car = 'mercedes'; //One of the exceptional ways of naming variables
+console.log($car);
+
+var _bike = 'sportsbike'; //One of the exceptional ways of naming variables
+console.log(_bike);
+
+......................................................................................
+
+ */
+
+ /*
+ ......................................................................................
+ 
+ // --------- VARIABLE MUTATION AND TYPE COERCION----------------
+ 
+
+ // TYPE COERCION
+ var firstName = 'Carol';
+ var age = 23;
+
+ console.log(firstName + ' ' +age);
+
+ var job, isMarried;
+ job = 'PentrationTester';
+ isMarried=false;
+
+ console.log(firstName + ' is ' +age + ' years old and works as a ' + job + '. is she married ? '+ isMarried);
+
+ // VARIABLE MUTATION
+
+ age='twenty three';
+ job='Software Engineer';
+
+ alert(firstName + ' is ' +age + ' years old and works as a ' + job + '. is she married ? '+ isMarried);
+
+ var lastName = prompt('Whats her Last Name? ');
+ console.log(firstName + ' ' + lastName);
+
+ ......................................................................................
+
+ */
+
+
+
+ /*
+
+ ......................................................................................
+
+ // BASIC OPERATORS
+
+ var yearJohn = 2020 - 24;
+ console.log(yearJohn);
+
+ var CarolAge = 2020 - 23;
+ console.log(CarolAge);
+
+//Another way to write this since we are using 2020 alot 
+
+var year=2020;
+var yearDave = year - 56;
+var yearMercy = year - 67;
+console.log('Dave was born in ' +yearDave+ ' and Mercy was born in ' +yearMercy);
+
+
+//LOGICAL OPERATORS
+
+var now, yearJohn, yearMark;
+now=2020;
+yearJohn=28;
+yearMark=33;
+
+bornJohn = now -yearJohn;
+bornMark = now -yearMark;
+
+console.log('John was born in ' +bornJohn+ ' and Mark was born in ' +bornMark);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+//where logic operator comes in
+var logicQN = yearJohn > yearMark;
+console.log(logicQN);
+
+//TYPE OF OPERATOR
+console.log(typeof logicQN); //boolean
+console.log(typeof year); //number 
+console.log(typeof 'Carol is a PenetrationTester'); //String
+
+var y;
+console.log(typeof y); //Undefined
+
+......................................................................................
+*/
+
+
+//OPERATOR PRECEDENCE
+
